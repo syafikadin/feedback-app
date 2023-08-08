be76d91bb5364d9e4982d520b66f98bd6026cc39
syafikadin <syafik.adin222@gmail.com>
2023-08-08 15:01:38 +0700

    Feedback App 3. Component, Props, & State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import FeedbackData from "./data/FeedbackData";
+import { useState } from "react";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [feedback, setFeedback] = useState(FeedbackData)
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are Your Sure Want to Delete?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
+
+    return (
+        <>
+            <Header />
+            <div className="container">
+                <FeedbackStats feedback={feedback}/>
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+            </div>
+        </>
+    )
 }
 
 export default App;
